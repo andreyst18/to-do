9d93161ff34b347d02f4fe76a8f53cc97edc89cb
andrey <andrejmax@yandex.ru>
2023-03-29 16:57:29 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    wwwww    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/main.css'
+</style>
